77b464d649b7449c784a8c9319ed9b0d1add81c1
ivan montañes <ivantuckp12345@gmail.com>
2024-04-29 22:51:58 -0300

    agregamos la funcionalidad de marcas tareas pendientes como realizadas

--- tp4.c
+++ tp4.c
@@ -18,18 +18,19 @@ Nodo *crearListaVacia();
 Nodo *crearNodo(int *id);
 void insertarNodo(Nodo **start, Nodo *nodo);
 void mostrarNodos(Nodo **start);
-Nodo *buscarNodo(Nodo **start, int idBuscado);
+Nodo buscarNodo(Nodo **start, int idBuscado);
 void eliminarNodo(Nodo **start,int idBuscado);
 
 
 int main()
 {
-    int id_autoincremental = 1000,opcion = 1,opcion2 = 1;
+    int id_autoincremental = 1000,opcion = 1,opcion2 = 1,id_buscado;
     Nodo *TareasPendientes = crearListaVacia();
     Nodo *TareasRealizadas = crearListaVacia();
-    while(opcion == 1 || opcion == 2){
+    while(opcion == 1 || opcion == 2 || opcion == 3){
         printf("1: cargar tareas\n");
         printf("2: mostrar tareas\n");
+        printf("3:modificar estado de las tareas\n");
         printf("eliga una opcion:");
         fflush(stdin);
         scanf("%d",&opcion);
@@ -47,6 +48,17 @@ int main()
             mostrarNodos(&TareasPendientes);
             printf("******TAREAS REALIZADAS******\n");
             mostrarNodos(&TareasRealizadas);
+            break;
+            case 3:
+            printf("******TAREAS PENDIENTES******\n");
+            mostrarNodos(&TareasPendientes);
+            printf("eliga una tarea para marcar como realizada por su id:");
+            fflush(stdin);
+            scanf("%d",&id_buscado);
+            Nodo nodoEncontrado = buscarNodo(&TareasPendientes,id_buscado);
+            insertarNodo(&TareasRealizadas,&nodoEncontrado);
+            eliminarNodo(&TareasPendientes,id_buscado);
+            break;
         }
     }
 }
@@ -99,13 +111,13 @@ void mostrarNodos(Nodo **start)
     }
 }
 
-Nodo *buscarNodo(Nodo **start, int idBuscado)
+Nodo buscarNodo(Nodo **start, int idBuscado)
 {
     Nodo * Aux = *start;
     while(Aux != NULL && Aux->T.TareaID != idBuscado){
         Aux = Aux->Siguiente;
     }
-    return Aux;
+    return *Aux;
 }
 
 void eliminarNodo(Nodo **start,int idBuscado)
@@ -115,7 +127,7 @@ void eliminarNodo(Nodo **start,int idBuscado)
         Aux = &(*Aux)->Siguiente;
     }
 
-    if(*Aux != NULL){
+    if(*Aux){
         Nodo * Temp = *Aux;
         *Aux = (*Aux)->Siguiente;
         free(Temp);
