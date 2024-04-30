6ed311062572e23609f84ca33fd4053532b9e58e
ivan montañes <ivantuckp12345@gmail.com>
2024-04-29 22:13:40 -0300

    version mejorada

--- tp4.c
+++ tp4.c
@@ -14,95 +14,100 @@ typedef struct Nodo
     struct Nodo *Siguiente;
 } Nodo;
 
-Nodo * CrearNodo(Tarea valor)
+Nodo *crearListaVacia();
+Nodo *crearNodo(int *id);
+void insertarNodo(Nodo **start, Nodo *nodo);
+void mostrarNodos(Nodo **start);
+Nodo *buscarNodo(Nodo **start, int idBuscado);
+void eliminarNodo(Nodo **start,int idBuscado);
+
+
+int main()
 {
-    //creamos el nodo utilizando memoria dinamica//
-    Nodo * NuevoNodo = (Nodo *)malloc(sizeof(Nodo));
-    //usamos el operador flecha pues es un puntero a estructura//
-    NuevoNodo->T = valor;
-    //asignamos en null el puntero (enlace)
-    NuevoNodo->Siguiente = NULL;
-    //retornamos el nuevo nodo//
-    return NuevoNodo;
+    int id_autoincremental = 1000,opcion = 1,opcion2 = 1;
+    Nodo *TareasPendientes = crearListaVacia();
+    Nodo *TareasRealizadas = crearListaVacia();
+    while(opcion == 1){
+        printf("1: cargar tareas\n");
+        printf("eliga una opcion:");
+        fflush(stdin);
+        scanf("%d",&opcion);
+        switch(opcion){
+            case 1:
+            while(opcion2 == 1){
+            insertarNodo(&TareasPendientes,crearNodo(&id_autoincremental));
+            printf("1: para cargar otra tarea\n");
+            fflush(stdin);
+            scanf("%d",&opcion2);
+            }
+            mostrarNodos(&TareasPendientes);
+            
+        }
+    }
 }
 
-Nodo * crearListaVacia()
+
+
+Nodo *crearListaVacia()
 {
     return NULL;
 }
 
-void InsertarNodo(Nodo ** start, Nodo *nuevoNodo)
+Nodo *crearNodo(int *id)
 {
-    nuevoNodo->Siguiente = *start;
-    *start = nuevoNodo;
-}
-
-void InsertarAlFinal(Nodo *start,Nodo *nuevoNodo)
-{
-    Nodo * Aux = start;
-    while(Aux->Siguiente){
-        Aux = Aux->Siguiente;
-    }
-    Aux->Siguiente = nuevoNodo;
-}
-
-
-Tarea crearTareas();
-void mostrarTareas(Tarea *tareas,int totales);
-void mostrarNodos(Nodo **nodos,int totales);
-
-int main()
-{
-    Nodo * start = crearListaVacia();
-    int opcion = 1,i = 0,totales = 1;
-    Tarea *tareasArreglo = (Tarea *)malloc(sizeof(Tarea) * totales);
-    Nodo ** nodosArreglo = (Nodo **)malloc(sizeof(Nodo*) * totales);
-    while(opcion == 1){
-    tareasArreglo[i]= crearTareas();
-    nodosArreglo[i] = CrearNodo(tareasArreglo[i]);
-    InsertarNodo(&start,nodosArreglo[i]);
-    printf("desea ingresar una nueva tarea?\n");
-    printf("1: cargar nueva tarea -- otro: salir\n");
-    fflush(stdin);
-    scanf("%d",&opcion);
-    if(opcion == 1){
-        totales++;
-        i++;
-        tareasArreglo = realloc(tareasArreglo,sizeof(tareasArreglo) * totales);
-    }
-    }
-    mostrarTareas(tareasArreglo,totales);
-    mostrarNodos(nodosArreglo,totales);
-}
-
-
-Tarea crearTareas()
-{
+    Nodo * nodoCreado = (Nodo *)malloc(sizeof(Nodo));
+    nodoCreado->T.TareaID = *id;
+    (*id)++;
     char buff[100];
-    Tarea tareaNueva;
-    tareaNueva.TareaID = 1000;
     printf("ingrese la descripcion de la tarea:");
     fflush(stdin);
     gets(buff);
-    tareaNueva.Descripcion = (char *)malloc((strlen(buff)+1) * sizeof(char));
-    strcpy(tareaNueva.Descripcion,buff);
-    tareaNueva.Duracion = rand() %91 + 10;
-    return tareaNueva;
+    nodoCreado->T.Descripcion = (char *)malloc((strlen(buff) + 1) * sizeof(char));
+    strcpy(nodoCreado->T.Descripcion,buff);
+    printf("ingrese la duracion de la tarea:");
+    fflush(stdin);
+    scanf("%d",&nodoCreado->T.Duracion);
+    nodoCreado->Siguiente = NULL;
+    return nodoCreado;
 }
 
-void mostrarTareas(Tarea *tareas,int totales)
+void insertarNodo(Nodo **start, Nodo *nodo)
 {
-    for(int i = 0; i < totales; i++){
-        printf("******TAREA %d ******\n",i+1);
+    nodo->Siguiente = *start;
+    *start = nodo;
+}
+
+void mostrarNodos(Nodo **start)
+{
+    Nodo *Aux = *start;
+    while(Aux != NULL){
+        printf("id: %d\n", Aux->T.TareaID);
         printf("descripcion: ");
-        puts(tareas[i].Descripcion);
+        puts(Aux->T.Descripcion);
+        printf("duracion: %d\n", Aux->T.Duracion);
+        Aux = Aux->Siguiente;
     }
 }
 
-void mostrarNodos(Nodo **nodos,int totales)
+Nodo *buscarNodo(Nodo **start, int idBuscado)
 {
-    for(int i = 0; i < totales; i++){
-        printf("******NODO %d ******\n",i+1);
-        puts(nodos[i]->T.Descripcion);
+    Nodo * Aux = *start;
+    while(Aux != NULL && Aux->T.TareaID != idBuscado){
+        Aux = Aux->Siguiente;
+    }
+    return Aux;
+}
+
+void eliminarNodo(Nodo **start,int idBuscado)
+{
+    Nodo ** Aux = start;
+    while(*Aux != NULL && (*Aux)->T.TareaID != idBuscado){
+        Aux = &(*Aux)->Siguiente;
+    }
+
+    if(*Aux != NULL){
+        Nodo * Temp = *Aux;
+        *Aux = (*Aux)->Siguiente;
+        free(Temp);
     }
 }
